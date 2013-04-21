8620b5f9965ea28238f5d93129b51d6eaaf8f590
Rui Mochila <rui.mochila@gmail.com>
2013-04-21 14:28:22 +0100

    Improvements to tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ class SessionsController < ApplicationController
 	
 
 	def create
-		user = User.find_by_email(params[:session][:email].downcase)
-  		if user && user.authenticate(params[:session][:password])
+		#user = User.find_by_email(params[:session][:email].downcase)
+		user = User.find_by_email(params[:email].downcase)
+  		#if user && user.authenticate(params[:session][:password])
+  		if user && user.authenticate(params[:password])
     		# Sign the user in and redirect to the user's show page.
     		sign_in user
       		redirect_to user
