e477511837796bc17aeaa531082f9887460cc7fb
Rui Mochila <rui.mochila@gmail.com>
2013-04-23 15:53:01 +0100

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@
 
 class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
+  has_many :microposts, dependent: :destroy
 
 	validates :name, 
 		presence: true, 
@@ -34,6 +35,10 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
